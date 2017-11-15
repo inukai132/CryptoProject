3aa9351260fe8775d62cb8a1bc2e056b6a37e25b
Joe Knight <knightjoe2222@gmail.com>
2017-11-15 16:31:25 -0500

    added form functionality based on requests from server

--- js/custom.js
+++ js/custom.js
@@ -1,40 +1,78 @@
+function renderHTML(response, direction){
+    var arr = response.split('\'');
+    var data = arr[3];
+    if(arr[1] == "data"){
+        switch(direction){
+            case 'e':
+                $("#ciphertext").val(data);
+                break;
+            case 'd':
+                $("#plaintext").val(data);
+                break;
+        }
+    }
+    else{ //padding error
+        alert(data);
+    }
+
+}
+function exists(data){
+    if(data.length > 0) return true;
+    alert("Please enter text");
+    return false;
+}
+function validKey(key){
+    if(key.length == 16 || key.length == 24 || key.length == 32) return true;
+    alert("AES key must be either 16, 24, or 32 bytes long");
+    return false;
+}
 $(document).ready(function() {
     $('#encrypt').on('submit', function(e) {
         e.preventDefault();
         var data = $('#plaintext').val();
         var key = $('#plaintext-key').val();
-        var direction = $('#dir1').val();
-        $.ajax({
-            url:'http://localhost:8080/encrypt',
-            method:'POST',
-            data:{
-                data:data,
-                key:key,
-                direction:direction
-            },
-            dataType:"json",
-            success:function() {
-                console.log('success'); 
-            }
-        });     
+        if(validKey(key) && exists(data)){
+            var direction = $('#dir1').val();
+            $.ajax({
+                url:'http://localhost:8080/encrypt',
+                method:'POST',
+                data:{
+                    data:data,
+                    key:key,
+                    direction:direction,
+                },
+                dataType:"json",
+                success:function(data, status) {
+                // for our purposes, all responses are errors.
+                },
+                error: function (err){ 
+                    renderHTML(err.responseText, direction);
+                }
+            });
+        }
     });
     $('#decrypt').on('submit', function(e) {
         e.preventDefault();
         var data = $('#ciphertext').val();
         var key = $('#ciphertext-key').val();
         var direction = $('#dir2').val();
-        $.ajax({
-            url:'http://localhost:8080/decrypt',
-            method:'POST',
-            data:{
-                data:data,
-                key:key,
-                direction:direction
-            },
-            dataType:"json",
-            success:function() {
-                console.log('success'); 
-            }
-        });     
+        if(validKey(key) && exists(data)){
+            $.ajax({
+                url:'http://localhost:8080/decrypt',
+                method:'POST',
+                data:{
+                    data:data,
+                    key:key,
+                    direction:direction
+                },
+                dataType:"json",
+                success:function(data, status) {
+                // for our purposes, all responses are errors.
+                },
+                error: function (err){ 
+                    renderHTML(err.responseText, direction);
+                }
+            });  
+        }   
     });
 }); 
